9711ae17758273a8ca51d802e9c4a8c54a1a098d
Nowak0 <kuubanowak@gmail.com>
2023-10-28 19:35:52 +0200

    Fetch API finished

--- fetchAPI.js
+++ fetchAPI.js
@@ -1,5 +1,21 @@
-const x=document.getElementById("printFetch")
-fetch('test.txt')
-    .then(response => response.text())
-    .then(data => x.innerHTML=data)
-    .catch(error => console.log('ERROR'))
+const printItems=document.getElementById('printFetch')
+const source = 'test.txt'
+const disableMessage = document.getElementById('JSnotworking')
+async function getData() {
+    disableMessage.style.display='none'
+    try {
+        const response = await fetch(source)
+        const data = await response.text()
+        if(response.status === 200) {
+            printItems.innerHTML=data
+        }
+        else {
+            printItems.innerHTML='Server Error ' + data.error.message
+        }
+    }
+    catch(error) {
+        printItems.innerHTML='Fetch Error ' + error.message
+    }
+}
+
+getData()
