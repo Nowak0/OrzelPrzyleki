b636c64f01851a0012b1a8ce0d721ebb5ffef03b
Nowak0 <kuubanowak@gmail.com>
2023-10-28 21:15:45 +0200

    fetch API
    added a button which turns on fetch API

--- fetchAPI.js
+++ fetchAPI.js
@@ -1,21 +1,19 @@
-const printItems=document.getElementById('printFetch')
-const source = 'test.txt'
-const disableMessage = document.getElementById('JSnotworking')
-async function getData() {
-    disableMessage.style.display='none'
+const standings = document.getElementById('standings');
+const source = 'test2.html';
+const disableMessage = document.getElementById('JSnotworking');
+
+async function getStandings() {
+    disableMessage.style.display = 'none';
     try {
-        const response = await fetch(source)
-        const data = await response.text()
-        if(response.status === 200) {
-            printItems.innerHTML=data
+        const response = await fetch(source);
+        const data = await response.text();
+        if (response.status === 200) {
+            standings.innerHTML = data;
         }
         else {
-            printItems.innerHTML='Server Error ' + data.error.message
+            standings.innerHTML = 'Server Error ' + data.error.message;
         }
-    }
-    catch(error) {
-        printItems.innerHTML='Fetch Error ' + error.message
+    } catch (error) {
+        standings.innerHTML = 'Fetch Error ' + error.message;
     }
 }
-
-getData()
