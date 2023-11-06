c3537e8524259196d79c6afb68cabddfcc5409eb
KobeJordan2423 <psscarpeta@gmail.com>
2023-11-06 19:13:42 +0100

    added slider and updated autocomplete

--- jquery.js
+++ jquery.js
@@ -8,19 +8,69 @@ $(document).ready(function() {
         $(this).css("background-color", "whitesmoke");
         $(this).css("color", "black");
     });
+    $("#todayDate").datepicker();
     var availableTags = [
-        "@gmail.com",
-        "@wp.pl",
-        "@onet.poczta.pl"
+        "gmail.com",
+        "wp.pl",
+        "onet.poczta.pl",
+        "interia.pl",
+        "yahoo.com"
       ];
-    $("#email").on("click", function() {
-        var input = $(this).val();
-        if(input=="@") {
-            $( "#email" ).autocomplete({
-                source: availableTags
-            });
+    function extractLast( userInput ) {
+        if (userInput.indexOf("@")!=-1){
+            var getEmail=userInput.split("@");
+            return getEmail[getEmail.length-1];
         }
-    })
+        return "";
+    }
+
+    $( "#email" )
+        // don't navigate away from the field on tab when selecting an item
+        /*
+        .bind( "keydown", function( event ) {
+            if ( event.keyCode === $.ui.keyCode.TAB &&
+                    $( this ).data( "autocomplete" ).menu.active ) {
+                event.preventDefault();
+            }
+        })
+        */
+        .autocomplete({
+            /*
+            source: function( request, response ) {
+                        var mail = extractLast(request.term);
+                        if(mail.length<1){return;}
+                        //var matcher = new RegExp( "^" + mail, "i" );
+                        /*
+                        response( $.grep( availableTags, function( item ){
+                            return matcher.test( item );
+                        }));
+                        */
+                       /*
+                       response(availableTags);
+             },
+            */
+            source: function (request, response) {
+                var mail = extractLast(request.term);
+                if(mail.length<1) {
+                    return;
+                }
+                response(availableTags);
+            },
+            focus: function() {
+                // prevent value inserted on focus
+                return false;
+            },
+            select: function( event, ui ) {
+                var terms = this.value.split(", ");
+                // remove the current input
+                var ml=terms[terms.length-1].split("@")[0];
+                terms.pop();
+                // add the selected item
+                terms.push( ml+"@"+ui.item.value );
+                this.value = terms.join( "" );
+                return false;
+            }
+        });
     $("#infoStadium").dialog({
         autoOpen: false,
         modal: true,
--- script.js
+++ script.js
@@ -1,33 +1,57 @@
 const disableMessage = document.getElementById('JSnotworking');
-const setStandingsVisibility=document.getElementById('standings');
+const setStandingsVisibility = document.getElementById('standings');
 disableMessage.style.display = 'none';
-standings.style.display='block';
-var countClicks = 0;
+standings.style.display = 'block';
+var countClicksFooter = 0;
+var countClicksSlider = 1;
 
 function changeFooter() {
-    countClicks+=1;
+    countClicksFooter += 1;
     const getFooter = document.getElementById('footer');
     const getFooterText = document.getElementById('footerText');
-    if (countClicks%2==1) {
-    getFooter.style.backgroundColor='darkred';
-    getFooter.style.color='black';
-    getFooterText.innerHTML = "Copyright &copy; 2023 - Jakub Nowak s197860";
-    } 
+    if (countClicksFooter % 2 == 1) {
+        getFooter.style.backgroundColor = 'darkred';
+        getFooter.style.color = 'black';
+        getFooterText.innerHTML = "Copyright &copy; 2023 - Jakub Nowak s197860";
+    }
     else {
-        getFooter.style.backgroundColor='navy';
-        getFooter.style.color='white';
+        getFooter.style.backgroundColor = 'navy';
+        getFooter.style.color = 'white';
         getFooterText.innerHTML = "Copyright &copy; 2023 - Jakub Nowak";
     }
 }
 
 function addSlider() {
     const divFetch = document.getElementById('publishFetch');
+    const divMain = document.getElementById('main');
     const divSlider = document.createElement('div');
-    const standings = document.getElementById('standings');
     divSlider.classList.add('slider');
-    const divContent = document.createTextNode("Siemano, narazie dodam tu tylko text");
-    divSlider.appendChild(divContent);
-    divFetch.insertBefore(divSlider,standings);
+    const image = document.createElement('img');
+    image.classList.add('sliderImg');
+    image.setAttribute('src', 'pics/rozgrzewka.jpeg');
+    image.setAttribute('alt', 'zdjecie naglowkowe 1');
+    image.addEventListener('click',changeSlider,false);
+    const createText = document.createElement('p');
+    createText.classList.add('sliderText');
+    const addText=document.createTextNode('haaaalo');
+    createText.appendChild(addText);
+    divSlider.appendChild(image);
+    divSlider.appendChild(createText);
+    divMain.insertBefore(divSlider, divFetch);
 }
 
 addSlider();
+
+function changeSlider() {
+    countClicksSlider += 1;
+    const image = document.getElementById('sliderImg');
+    if (countClicksSlider == 1) {
+        image.setAttribute('src', 'pics/rozgrzewka.jpeg');
+        image.setAttribute('alt', 'zdjecie naglowkowe 1');
+    }
+    if (countClicksSlider == 2) {
+        image.setAttribute('src', 'pics/objecia_po_meczu.jpeg');
+        image.setAttribute('alt', 'zdjecie naglowkowe 2');
+        countClicksSlider = 0;
+    }
+}
