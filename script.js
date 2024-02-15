24519fbc2832f668dfde1ed4fdf2d5e453c8bea9
Nowak0 <80354460+Nowak0@users.noreply.github.com>
2024-02-15 16:58:54 +0100

    update

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
-const disableMessage = document.getElementById('JSnotworking');
-const setStandingsVisibility = document.getElementById('standings');
-const goToTop = document.getElementById('goToTop');
-disableMessage.style.display = 'none';
-setStandingsVisibility.style.display = 'block';
 let countClicksSlider = 1;
 let timeout;
 
 function startTimeout() {
     timeout=window.setTimeout(changeSlider,5000);
-
 }
+
+
+function resetAnimation() {
+    const image=document.getElementById('sliderImg');
+    const text = document.getElementById('sliderItems');
+    image.style.animation='none';
+    image.offsetHeight;
+    image.style.animation=null;
+    text.style.animation='none';
+    text.offsetHeight;
+    text.style.animation=null;
+}
+
+
+function sliderButtonsFunctions() {
+    const goRight = document.getElementById('goRight');
+    const goLeft = document.getElementById('goLeft');
+    goLeft.onclick = function() {
+        if(countClicksSlider>=2) {countClicksSlider-=2;}
+        else if(countClicksSlider>3) {countClicksSlider=0;}
+        else {countClicksSlider+=1;}
+        changeSlider();
+    }
+    goRight.onclick = function() {
+        changeSlider();
+    }
+    startTimeout();
+}
+
 
 function addSlider() {
     const divFetch = document.getElementById('publishFetch');
@@ -48,49 +71,52 @@ function addSlider() {
     startTimeout();
 }
 
-addSlider();
+
+//addSlider();
+
 
 function changeSlider() {
     countClicksSlider+=1;
     const image = document.getElementById('sliderImg');
-    const text = document.getElementById('sliderText');
+    const smallText = document.getElementById('smallSliderText');
+    const bigText = document.getElementById('bigSliderText');
+    const button = document.getElementById('sliderButton');
+
     clearTimeout(timeout);
     resetAnimation();
     if (countClicksSlider == 1) {
         image.setAttribute('src', 'pics/rozgrzewkaSlider.jpeg');
         image.setAttribute('alt', 'zdjecie naglowkowe 1');
-        text.innerHTML="Zawsze walczymy do końca!<br> Dołącz do nas!"
+        smallText.innerHTML = "Drużyna seniorów";
+        bigText.innerHTML="Zawsze walczymy do końca!<br> Dołącz do nas!";
+        button.innerHTML = "terminarz";
     }
     if (countClicksSlider == 2) {
         image.setAttribute('src', 'pics/juniorzySlider.jpg');
         image.setAttribute('alt', 'zdjecie naglowkowe 2');
-        text.innerHTML="Szkolimy małe Orlęta";
+        smallText.innerHTML = "Drużyna juniorów";
+        bigText.innerHTML="Szkolimy małe Orlęta";
+        button.innerHTML = "dowiedz się więcej";
     }
     if (countClicksSlider == 3) {
         image.setAttribute('src','pics/wsparcie_slider.jpg');
         image.setAttribute('alt','zdjecie naglowkowe 3');
-        text.innerHTML="Skontaktuj się z nami"
+        smallText.innerHTML = "Współpraca";
+        bigText.innerHTML="Skontaktuj się z nami";
+        button.innerHTML = "dowiedz się więcej";
         countClicksSlider = 0;
     }
     startTimeout();
 }
 
-function resetAnimation() {
-    const image=document.getElementById('sliderImg');
-    const text = document.getElementById('sliderText');
-    image.style.animation='none';
-    image.offsetHeight;
-    image.style.animation=null;
-    text.style.animation='none';
-    text.offsetHeight;
-    text.style.animation=null;
-}
 
 window.onscroll = function() {
     scrollFunction();
 }
 
+
 function scrollFunction() {
+    const goToTop = document.getElementById('goToTop');
     if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
         goToTop.style.display = 'block';
     }
@@ -98,6 +124,20 @@ function scrollFunction() {
         goToTop.style.display = 'none';
     }
 }
+
+
+function sliderButtonAction() {
+    if (countClicksSlider == 1) {
+        window.location.href="";
+    }
+    if (countClicksSlider == 2) {
+        window.location.href="";
+    }
+    if (countClicksSlider == 3) {
+        window.location.href="";
+    }
+}
+
 
 $(document).ready(function(){
     $("a").on('click', function() {
@@ -108,3 +148,6 @@ $(document).ready(function(){
         }
     });
   });
+
+
+sliderButtonsFunctions();
