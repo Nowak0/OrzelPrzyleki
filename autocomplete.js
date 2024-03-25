cd8d6a486168532781a72e71b788ad72d28001ac
Nowak0 <kuubanowak@gmail.com>
2024-03-25 13:45:32 +0100

    update

--- autocomplete.js
+++ autocomplete.js
@@ -52,30 +52,3 @@ $(document).ready(function() {
             }
         });
 });
-
-/*
-$(document).ready(function(){
-    // Add smooth scrolling to all links
-    $("#goToTop").on('click', function(event) {
-  
-      // Make sure this.hash has a value before overriding default behavior
-      var action=this.hash
-      $('html, body').animate({
-        scrollTop: 0
-      }, 800);
-      if (action !== "") {
-        // Prevent default anchor click behavior
-        //event.preventDefault();
-  
-        // Store hash
-        //var hash = this.hash;
-  
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-       
-      } // End if
-    });
-  });
-  //scrollTop: $(action).offset().top
-
-  */
