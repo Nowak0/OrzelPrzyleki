e45364ce2ac5eeeaacf6fb061af084fe98605697
Nowak0 <kuubanowak@gmail.com>
2024-08-20 15:53:57 +0200

    update tmp

--- js/adjust_iframe_size.js
+++ js/adjust_iframe_size.js
@@ -1,21 +1,42 @@
 function adjustMap() {
     const map = document.getElementById('map');
-    const ratio = 0.4;
-    const width = window.innerWidth - 20;
-    const height = width * ratio;
-    map.style.width = `${width}px`;
-    map.style.height = `${height}px`;
+    if (map) {
+        const ratio = 0.4;
+        const width = window.innerWidth - 20;
+        const height = width * ratio;
+        map.style.width = `${width}px`;
+        map.style.height = `${height}px`;
+    }
+}
+
+function setAdditionalHeight() {
+    console.log("jest");
+    if (window.innerWidth > 1000) {
+        return 0.1 * window.innerWidth;
+    }
+    else {
+        return 0.2 * window.innerWidth
+    }
 }
 
 function adjustSponsors() {
     const stripes = document.querySelectorAll('.adjustable'); 
-    stripes.forEach((stripe, index) => {
-        const iframeDocument = stripe.contentDocument || stripe.contentWindow.document;
-        stripe.style.height = iframeDocument.body.scrollHeight + 200 + 'px';
-    });
+
+    if (stripes) {
+        const additionalHeight = setAdditionalHeight();
+        console.log(additionalHeight)
+        stripes.forEach((stripe, index) => {
+            const iframeDocument = stripe.contentDocument || stripe.contentWindow.document;
+            stripe.style.height = iframeDocument.body.scrollHeight + additionalHeight + 'px';
+        });
+        console.log("haha");
+    }
 }
 
-window.onload = adjustMap;
-window.onresize = adjustMap;
-window.onload = adjustSponsors;
-window.onresize = adjustSponsors;
+function runFunctions() {
+    adjustSponsors();
+    adjustMap();
+}
+
+window.onload = runFunctions;
+window.addEventListener('resize', adjustMap);
