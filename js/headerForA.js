9b7e4ce356a565ca63995de388971be6f9fa4a68
Nowak0 <kuubanowak@gmail.com>
2025-03-03 12:46:02 +0100

    responsive menu

---
+++ js/headerForA.js
@@ -0,0 +1,94 @@
+function createHeader() {
+    const header = document.createElement('header');
+  
+    const logoContainer = document.createElement('div');
+    logoContainer.classList.add('logo');
+    const logoLink = document.createElement('a');
+    logoLink.href = 'index.html';
+    const logoImage = document.createElement('img');
+    logoImage.src = 'pics/svg_czysty_orzel_logo.svg';
+    logoImage.alt = 'logo Orzeł Przyłęki';
+    logoLink.appendChild(logoImage);
+    logoContainer.appendChild(logoLink);
+  
+    const nav = document.createElement('nav');
+    nav.classList.add('topnav');
+    const ul = document.createElement('ul');
+
+    const mobileLi = document.createElement('li');
+    const mobileSubnav = document.createElement('div');
+    mobileSubnav.classList.add('subnav');
+    const mobileIcon = document.createElement('a');
+    mobileIcon.href = 'javascript:void(0);';
+    mobileIcon.classList.add('subnavHeader', 'icon');
+    mobileIcon.innerHTML = '<i class="fa fa-bars"></i>';
+    mobileIcon.onclick = function () { mobileResponsiveMenu(); };
+    mobileSubnav.appendChild(mobileIcon);
+    mobileLi.appendChild(mobileSubnav);
+    ul.appendChild(mobileLi);
+  
+    const navItems = [
+      {
+        title: 'SENIORZY &#x2335;',
+        links: [
+          { text: 'Drużyna', href: 'team.html' }
+        ]
+      },
+      {
+        title: 'AKADEMIA &#x2335;',
+        links: [
+          { text: 'Orlęta', href: 'academy.html' },
+          { text: 'Galeria', href: 'galleryAcademy.html'}
+        ]
+      },
+      {
+        title: 'WSPARCIE &#x2335;',
+        links: [
+          { text: 'Sponsorzy', href: 'sponsors.html' },
+          { text: 'Gmina Białe Błota', href: 'biale_blota.html' }
+        ]
+      },
+      {
+        title: 'KONTAKT &#x2335;',
+        links: [
+          { text: 'Napisz do nas', href: 'contact.html' },
+          { text: 'Nasz Facebook', href: 'https://www.facebook.com/OrzelPrzyleki', target: '_blank' }
+        ]
+      }
+    ];
+  
+    navItems.forEach(item => {
+      const li = document.createElement('li');
+      const subnav = document.createElement('div');
+      subnav.classList.add('subnav');
+      const subnavHeader = document.createElement('a');
+      subnavHeader.classList.add('subnavHeader');
+      subnavHeader.innerHTML = item.title;
+      const subnavContent = document.createElement('div');
+      subnavContent.classList.add('subnavContent');
+      
+      item.links.forEach(link => {
+        const a = document.createElement('a');
+        a.href = link.href;
+        if (link.target) a.target = link.target;
+        a.innerText = link.text;
+        subnavContent.appendChild(a);
+      });
+  
+      subnav.appendChild(subnavHeader);
+      subnav.appendChild(subnavContent);
+      li.appendChild(subnav);
+      ul.appendChild(li);
+    });
+
+    
+
+    nav.appendChild(ul);
+    header.appendChild(logoContainer);
+    header.appendChild(nav);
+
+    document.body.prepend(header);
+  }
+  
+  document.addEventListener('DOMContentLoaded', createHeader);
+  
--- js/mobileResponsiveMenu.js
+++ js/mobileResponsiveMenu.js
@@ -1,13 +1,8 @@
-function mobileResponsiveMenu() {
-    var navElements = document.querySelectorAll(".topnav");
+document.addEventListener("DOMContentLoaded", function () {
+    let menuToggle = document.querySelector('.menuToggle');
+    let header = document.querySelector('header');
 
-    navElements.forEach(function(this_nav) {
-      this_nav.classList.toggle("mobileResponsiveMenu");
-      if(this_nav.classList.contains("mobileResponsiveMenu")) {
-        this_nav.style.paddingBottom = "50px";
-      } else {
-        this_nav.style.paddingBottom = "0px";
-      }
-    });
-  }
-  
+    menuToggle.onclick = function() {
+        header.classList.toggle('responsiveMenu');
+    };
+});
